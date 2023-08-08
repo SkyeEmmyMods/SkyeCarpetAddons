39392c2ff75d513ee83a92eef5d3c2745f0d948b
FubyCutie <roset280905@gmail.com>
2023-08-08 22:19:38 +0100

    imported carpet

--- src/main/kotlin/uk/fuby/skyecarpetaddons/SkyeCarpetAddons.kt
+++ src/main/kotlin/uk/fuby/skyecarpetaddons/SkyeCarpetAddons.kt
@@ -7,9 +7,6 @@ object SkyeCarpetAddons : ModInitializer {
     private val logger = LoggerFactory.getLogger("skye-carpet-addons")
 
 	override fun onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		logger.info("Hello Fabric world!")
+
 	}
 }
